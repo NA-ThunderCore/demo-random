73865b307d872ee5ff2c472ea1214987c5a4b9df
NA <na.chuang@thundercore.tw>
2023-06-13 11:26:34 +0800

    add verify contract example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 import "tt-hardhat";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  etherscan: {
+    apiKey: {
+      "thunder-testnet": "unused",
+    },
+    customChains: [
+      {
+       network: "thunder-testnet",
+        chainId: 18,
+        urls: {
+          apiURL: "https://explorer-testnet.thundercore.com/api",
+          browserURL: "https://explorer-testnet.thundercore.com",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
